7da36da51670755fb67a6fdaca22cf4f833e0931
Jose Arias <jose.ricardo.arias@estudiantat.upc.edu>
2026-04-02 22:23:38 +0200

    q3

--- Assignment01/Q3/Solution.R
+++ Assignment01/Q3/Solution.R
@@ -25,26 +25,36 @@ air_data <- air_data |>
 air_data$EngineType <- as.factor(air_data$EngineType)
 
 # Price is generally right-skewed data; a log() transformation helps to normalize the data
-# Also in linear regression we want to keep homoscedasticity  ???
+hist(air_data$Price)
 air_data$Price <- log(air_data$Price)
+hist(air_data$Price)
 
 # Numerical vars only
 num_vars <- names(air_data)[sapply(air_data, is.numeric)]
+par(mfrow = c(3, 3))
+for (var in num_vars) {
+    plot(air_data[[var]], air_data$Price, 
+         xlab = var, ylab = "Price", 
+         main = paste("Price vs", var),
+         pch = 19, col = "lightgreen")
+    # Add regression line
+    abline(lm(air_data$Price ~ air_data[[var]]), col = "red", lwd = 2)
+}
+par(mfrow = c(1, 1))
+
+# According to plot analysis, we are selecting variables NumberOfEngines, Capacity, and RangeKm
+# as best candidates for the linear model bc those show the most linear relationship with Price
+
 numeric_air_data <- air_data[, num_vars]
-
 str(numeric_air_data)
-plot(numeric_air_data)
 
-# From the plot, we can see that RangeKm and Capacity are the variables with the highest correlation vs Price
 # Next, we use the Correlation Matrix to confirm our assumption
-
-# Correlation Matrix
+# Correlation with Price variable (Highest to Lowest)
 cor_matrix <- cor(numeric_air_data)
 cor_matrix
-# Correlation with Price variable (Highest to Lowest)
 sort(cor_matrix[, "Price"], decreasing = TRUE)
 
-# Correlation Matrix tell us that NumberofEngines, Capacity and RangeKm are the top 3 
+# Correlation Matrix tell us indeed that NumberofEngines, Capacity and RangeKm are the top 3 
 # with the highest correlation
 
 # Linear Models
@@ -60,61 +70,56 @@ par(mfrow = c(1, 1))
 
 
 # The plots suggest that:
-# - NumberofEngines is showing heteroscedasticity, meaning the linear model is not working
-# good for higher prices
-# In the case of Capacity and RangeKm, the model may need to include a quadratic term
+# - NumberofEngines is showing a nearly linear relationship with Price
+# - In the case of Capacity and RangeKm, the model may need to include a quadratic term
 
-# Breusch Pagan to confirm this!
-bptest(slr.engines)
-bptest(slr.capacity)
-bptest(slr.range)
 
-# After running BP test, the 3 models show significant heteroscedasticity
-# We decided to discard the first one. The other 2 suggest a quadratic relation
+#Capacity2 <- (numeric_air_data$Capacity) ^ 2
+#slr.capacity_2 <- lm(Price ~ Capacity + Capacity2, data = numeric_air_data)
+#summary(slr.capacity_2)
+#RangeKm2 <- (numeric_air_data$RangeKm) ^ 2
+#slr.range_2 <- lm(Price ~ RangeKm + RangeKm2, data = numeric_air_data)
+#summary(slr.range_2)
 
-Capacity2 <- (numeric_air_data$Capacity) ^ 2
-slr.capacity_2 <- lm(Price ~ Capacity + Capacity2, data = numeric_air_data)
-summary(slr.capacity_2)
-RangeKm2 <- (numeric_air_data$RangeKm) ^ 2
-slr.range_2 <- lm(Price ~ RangeKm + RangeKm2, data = numeric_air_data)
-summary(slr.range_2)
-
-par(mfrow = c(1, 2))
-plot(slr.capacity_2, which = 1, main = "Capacity")
-plot(slr.range_2, which = 1, main = "RangeKm")
-par(mfrow = c(1, 1))
+#par(mfrow = c(1, 2))
+#plot(slr.capacity_2, which = 1, main = "Capacity")
+#plot(slr.range_2, which = 1, main = "RangeKm")
+#par(mfrow = c(1, 1))
 
 # Breusch Pagan with quadratic terms
 # TODO: BPTests keep failing after adding a quadratic term :(
-bptest(slr.capacity_2)
-bptest(slr.range_2)
+#bptest(slr.capacity_2)
+#bptest(slr.range_2)
 
 # Compare R-squared
-summary(slr.range_2)$r.squared
-summary(slr.capacity_2)$r.squared
-# Selecting RangeKm for now as it yields the higher R2
+#summary(slr.range_2)$r.squared
+#summary(slr.capacity_2)$r.squared
+
 
 # Regression Assumptions
 
 # Normality of the Error Term
-qqnorm(residuals(slr.range_2))
+qqnorm(residuals(slr.engines))
 # Using Histogram
-hist(residuals(slr.range_2))
+hist(residuals(slr.engines))
 
 # Homogeneity of Variance
-plot(residuals(slr.range_2))
+plot(residuals(slr.engines))
 # Breusch Pagan
-bptest(slr.range_2)
+bptest(slr.engines)
+
+# After running BP test, the model show significant heteroscedasticity
 
 # Independence of errors
-dwtest(slr.range_2, alternative = "two.sided")
+dwtest(slr.engines, alternative = "two.sided")
 
 
-# We select `slr.range_2` model, and therefore, RangeKm as the best predictor for Price 
-# because it has hight Correlation vs Price and the highest R-squared value,
-# meaning it explains the highest proportion of the variance in airplane prices
+# We select `slr.engines` model, and therefore, NumberofEngines as the best single predictor
+# because it shows a linear high correlation vs Price, and also high R-squared value,
+# meaning it alone explains the highest proportion of the variance in airplane prices
 
-summary(slr.range_2)
+summary(slr.engines)
+# Selecting NumberofEngines for now with R2 = 0.7792
 
 # Intercept ($\beta_0$): Estimated baseline Price of an airplane
 # Slope for RangeKm ($\beta_1$): Estimated change in the dependent variable for every 1-unit increase in the independent variable
@@ -127,23 +132,35 @@ summary(slr.range_2)
 # the simple linear regression model that you fit in (a). Which one is a better model? Why? 
 # ---------------------------------------------------------------------------------------
 
-regmodel.b <- lm(Price ~ Capacity + RangeKm, data = numeric_air_data)
-summary(regmodel.b)
+regmodel.b1 <- lm(Price ~ NumberofEngines + Capacity, data = numeric_air_data)
+summary(regmodel.b1)
+regmodel.b2 <- lm(Price ~ NumberofEngines + RangeKm, data = numeric_air_data)
+summary(regmodel.b2)
+
+# model b1 yields R2 = 0.9815 vs model b2 yields R2 = 0.9699
 
 # Regression Assumptions
 
 # Normality of the Error Term
-qqnorm(residuals(regmodel.b))
+qqnorm(residuals(regmodel.b1))
 # Using Histogram
-hist(residuals(regmodel.b))
+hist(residuals(regmodel.b1))
 
 # Homogeneity of Variance
-plot(residuals(regmodel.b))
+plot(residuals(regmodel.b1))
 # Breusch Pagan
-bptest(regmodel.b)
+bptest(regmodel.b1)
 
 # Independence of errors
-dwtest(regmodel.b, alternative = "two.sided")
+dwtest(regmodel.b1, alternative = "two.sided")
+
+
+# Compare model b with model a
+anova(slr.engines, regmodel.b1)
+summary(regmodel.b1)
+
+# Adding Capacity to the model decreases significantly the Residual Sum of Squares,
+# meaning the model improves by adding this new variable
 
 
 # ------------------------------------------ C ------------------------------------------
@@ -159,27 +176,40 @@ numeric_air_data$ModelCat <- as.factor(
 )
 summary(numeric_air_data)
 
-regmodel.c <- lm(Price ~ Capacity + RangeKm + ModelCat, data = numeric_air_data)
-summary(regmodel.c)
+regmodel.c1 <- lm(Price ~ NumberofEngines + Capacity + ModelCat, data = numeric_air_data)
+summary(regmodel.c1)
+regmodel.c2 <- lm(Price ~ NumberofEngines + RangeKm + ModelCat, data = numeric_air_data)
+summary(regmodel.c2)
+
+# Compare model c to models in b
+anova(regmodel.b1, regmodel.c1)
+anova(regmodel.b2, regmodel.c2)
+
+bptest(regmodel.c1)
+bptest(regmodel.c2)
 
 
 # ------------------------------------------ D ------------------------------------------
 # Test the validity of the final model that you choose.
 # ---------------------------------------------------------------------------------------
 
+# Choosing model c2, lm(Price ~ NumberofEngines + RangeKm + ModelCat)
+
+
 # Regression Assumptions
 
 # Normality of the Error Term
-qqnorm(residuals(regmodel.c))
+qqnorm(residuals(regmodel.c2))
 # Using Histogram
-hist(residuals(regmodel.c))
+hist(residuals(regmodel.c2))
 
 # Homogeneity of Variance
-plot(residuals(regmodel.c))
+plot(residuals(regmodel.c2))
 # Breusch Pagan
-bptest(regmodel.c)
+bptest(regmodel.c2)
 
 # Independence of errors
-dwtest(regmodel.c, alternative = "two.sided")
+dwtest(regmodel.c2, alternative = "two.sided")
 
+summary(regmodel.c2)
 
